299bfcdacd2b74b52b567b5693bdb26417291079
rajkumar <rajkumar4152@gmail.com>
2019-07-11 19:44:11 +0530

    after adding header to the code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route,Switch} from 'react-router-dom';
+import Header from './components/header/header.component';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
@@ -10,6 +11,7 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header/>
       <Switch> 
       <Route path ='/shop/' component ={ShopPage}/>   
       <Route path='/' component={HomePage}/>
