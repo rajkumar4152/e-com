36ade731965be915fe18373637e73448e955e9ea
rajkumar <rajkumar4152@gmail.com>
2019-07-17 19:57:44 +0530

    added redux and finished

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import {createStore,applyMiddleware} from 'redux';
+
+import logger from 'redux-logger';
+
+import rootReducer from './root-reducer';
+
+const middelwares = [logger];
+
+const store= createStore(rootReducer,applyMiddleware(...middelwares));
+
+export default store;
+
