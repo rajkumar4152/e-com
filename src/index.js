9862ef63da0bb1310bb927429edce0f1ad1f3d39
rajkumar <rajkumar4152@gmail.com>
2019-07-18 19:58:26 +0530

    added after grouping using utils.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+
 import store from './redux/store';
 
 ReactDOM.render(
