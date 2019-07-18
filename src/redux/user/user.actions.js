51c2e7479c0f96cc278b51dd0b3d6ceb7b0faac4
rajkumar <rajkumar4152@gmail.com>
2019-07-18 10:58:39 +0530

    added cart button and drop-down

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,7 @@
 
 import { UserActionTypes } from './user.types'
 
-export const setCurrentUser = user => ({
+export  const setCurrentUser = user => ({
     type:UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
